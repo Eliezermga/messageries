7e44b1ad9037dccdfd50e9fed8db1c92326bf1af
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-05-16 09:35:26 +0200

    chat

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
+const io = require("sokcket.io")(server);
+
+app.use(express.static(path.join(__dirname+"/public")));
+
+io.on("connection", function(socket){
+    socket.on("newuser",function(username){
+        socket.broadcast.emit("update", username + "joined the conversation");
+    });
+    socket.on("exituser", function(username){
+        socket.broadcast.emit("uptade", username +"left the conversation");
+    });
+    socket.on("chat", function(message){
+        socket.broadcast.emit("chat", message);
+    });
+});
+
 app.use(express.static(path.join(__dirname+"public")));
 
 server.listen(5000);
